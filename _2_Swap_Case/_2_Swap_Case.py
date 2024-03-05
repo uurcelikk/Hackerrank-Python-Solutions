921525a24f9ec764db84a15d03a21e24eb527a40
Uğur Çelik <79105830+uurcelikk@users.noreply.github.com>
2024-03-05 15:26:27 +0300

    Updated _2_Swap_case

--- _2_Swap_Case/_2_Swap_Case.py
+++ _2_Swap_Case/_2_Swap_Case.py
@@ -3,3 +3,27 @@ Question: Swap Case
 Given a string, convert it into a string with upper case and lower case swapped.
 Return the modified string.
 """
+def swap_case(s):
+    swapped = ''
+    for char in s:
+        if char.islower():
+            swapped += char.upper()
+        elif char.isupper():
+            swapped += char.lower()
+        else:
+            swapped += char
+    return swapped
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+#Solution2
+# def swap_case(s):
+#     return s.swapcase()
+
+# if __name__ == '__main__':
+#     s = input()
+#     result = swap_case(s)
+#     print(result)
