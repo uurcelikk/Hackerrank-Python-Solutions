6f89d9dff128111acda2a37f8651a2ccc87ddeb9
Uğur Çelik <79105830+uurcelikk@users.noreply.github.com>
2024-03-08 23:52:54 +0300

    Create

---
+++ _6_String_Validators/_6_String_Validators.py
@@ -0,0 +1,13 @@
+"""
+Question: String Validators
+You are given a string S. Your task is to find out if the string S contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
+"""
+
+if __name__ == '__main__':
+    s = input().strip()
+
+    print(any(c.isalnum() for c in s))
+    print(any(c.isalpha() for c in s))
+    print(any(c.isdigit() for c in s))
+    print(any(c.islower() for c in s))
+    print(any(c.isupper() for c in s))
