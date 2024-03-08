e14cd2607cadda01a6f676710498b3cff25f5887
Uğur Çelik <79105830+uurcelikk@users.noreply.github.com>
2024-03-08 23:50:03 +0300

    Create _5_Find_a_Sting.py

---
+++ _5_Find_a_String/_5_Find_a_String.py
@@ -0,0 +1,19 @@
+"""
+Question: Find a string
+You are given a string and a substring. You have to find the number of times that the substring occurs in the given string.
+
+"""
+
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string) - len(sub_string) + 1):
+        if string[i:i+len(sub_string)] == sub_string:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
